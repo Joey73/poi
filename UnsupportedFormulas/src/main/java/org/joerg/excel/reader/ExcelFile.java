34fec72cd3544cd6ce5faee640e9897bd2181966
IV#11GJ <IV#11GJ@KOENB12758.koeln.ads.risnet.de>
2015-01-02 15:08:21 +0100

    Formated

--- UnsupportedFormulas/src/main/java/org/joerg/excel/reader/ExcelFile.java
+++ UnsupportedFormulas/src/main/java/org/joerg/excel/reader/ExcelFile.java
@@ -19,7 +19,7 @@ import org.joerg.excel.ext.PoiExtension;
 public class ExcelFile {
 	private static XSSFWorkbook wb = null;
 	private static XSSFSheet currentSheet = null;
-	
+
 	public ExcelFile() {
 		wb = getExcelWorkbook("Testtabelle.xlsx");
 	}
@@ -28,42 +28,42 @@ public class ExcelFile {
 		wb = getExcelWorkbook(filePath);
 	}
 
-	public Collection<String> getUnsupportedFunctionsOfAllSheets(){
+	public Collection<String> getUnsupportedFunctionsOfAllSheets() {
 		Collection<String> functionList = new ArrayList<String>();
-		XSSFSheet sheet;		
-		
+		XSSFSheet sheet;
+
 		int numberOfSheets = wb.getNumberOfSheets();
 		for (int i = 0; i < numberOfSheets; i++) {
 			sheet = wb.getSheetAt(i);
 			functionList.addAll(getUnsupportedFunctionsOfSheet(sheet));
 		}
-		
+
 		return filterDuplicates(functionList);
 	}
 
-	public void listUnsupportedFunctionsOfAllSheets(){
+	public void listUnsupportedFunctionsOfAllSheets() {
 		System.out.println("--- Unsupported Functions of all sheets ---");
 		for (String unsupportedFunction : getUnsupportedFunctionsOfAllSheets()) {
 			System.out.println(unsupportedFunction);
 		}
 	}
-	
-	public Collection<String> filterDuplicates(Collection<String> list){
+
+	public Collection<String> filterDuplicates(Collection<String> list) {
 		Collection<String> resultList = new ArrayList<String>();
 		Set<String> tmpSet = new HashSet<String>();
-		
+
 		for (String listEntry : list) {
-			if(tmpSet.add(listEntry)){
-				if(!listEntry.equals("N")){
+			if (tmpSet.add(listEntry)) {
+				if (!listEntry.equals("N")) {
 					resultList.add(listEntry);
 				}
 			}
 		}
-		
+
 		return resultList;
 	}
-	
-	public Collection<String> getCellInfoOfUnsupportedFunctionsOfAllSheets(){
+
+	public Collection<String> getCellInfoOfUnsupportedFunctionsOfAllSheets() {
 		Collection<String> cellInfoList = new ArrayList<String>();
 		XSSFSheet sheet;
 		int numberOfSheets = wb.getNumberOfSheets();
@@ -76,62 +76,63 @@ public class ExcelFile {
 		}
 		return cellInfoList;
 	}
-	
-	public void listCellInfoOfUnsupportedFunctionsOfAllSheets(){
+
+	public void listCellInfoOfUnsupportedFunctionsOfAllSheets() {
 		System.out.println("\n--- Cell info of all unsupported functions ---");
 		Collection<String> cellInfoOfUnsupportedFunctionsOfAllSheets = getCellInfoOfUnsupportedFunctionsOfAllSheets();
-		
+
 		for (String cellInfo : cellInfoOfUnsupportedFunctionsOfAllSheets) {
 			System.out.println(cellInfo);
 		}
 	}
-	
-	public Collection<String> getUnsupportedFunctionsOfSheet(XSSFSheet sheet){
+
+	public Collection<String> getUnsupportedFunctionsOfSheet(XSSFSheet sheet) {
 		Collection<String> functionList = new ArrayList<String>();
 		Collection<String> unsupportedFunctionsOfPoi = PoiExtension.getUnsupportedFunctionsOfPoi();
 		Collection<String> allSheetFunctions = getAllSheetFunctions(sheet);
 
 		for (String sheetFunction : allSheetFunctions) {
 			for (String unsupportedFunctionOfPoi : unsupportedFunctionsOfPoi) {
-				if(sheetFunction.contains(unsupportedFunctionOfPoi)){
-					if(!functionList.contains(unsupportedFunctionOfPoi)){
+				if (sheetFunction.contains(unsupportedFunctionOfPoi)) {
+					if (!functionList.contains(unsupportedFunctionOfPoi)) {
 						functionList.add(unsupportedFunctionOfPoi);
-						//functionList.add(unsupportedFunctionOfPoi + " - in: " + sheetFunction);
+						// functionList.add(unsupportedFunctionOfPoi + " - in: " + sheetFunction);
 					}
 				}
 			}
 		}
-		
+
 		return functionList;
 	}
 
-	public Collection<String> getCellInfoOfUnsupportedFunctionsOfSheet(XSSFSheet sheet, String function){
+	public Collection<String> getCellInfoOfUnsupportedFunctionsOfSheet(XSSFSheet sheet, String function) {
 		Collection<String> cellInfo = new ArrayList<String>();
 
 		for (Row row : sheet) {
 			for (Cell cell : row) {
 				if (cell != null) {
 					if (Cell.CELL_TYPE_FORMULA == cell.getCellType()) {
-						if(cell.getCellFormula().contains(function)){
-							cellInfo.add("Formula: " + function + ", Sheet: " + sheet.getSheetName() + ", Row: " + (cell.getRowIndex() + 1) + ", Column: " + (cell.getColumnIndex() + 1) + ", CellContent: " + cell.getCellFormula());
+						if (cell.getCellFormula().contains(function)) {
+							cellInfo.add("Formula: " + function + ", Sheet: " + sheet.getSheetName() + ", Row: " + (cell.getRowIndex() + 1) + ", Column: "
+									+ (cell.getColumnIndex() + 1) + ", CellContent: " + cell.getCellFormula());
 						}
 					}
 				}
 			}
 		}
-		
+
 		return cellInfo;
 	}
 
-	public void listUnsupportedFunctionsOfSheet(XSSFSheet sheet){
+	public void listUnsupportedFunctionsOfSheet(XSSFSheet sheet) {
 		Collection<String> unsupportedFunctionsOfSheet = getUnsupportedFunctionsOfSheet(sheet);
 		System.out.println("Unsupported Functions of Sheet:");
 		for (String unsupportedFunctionOfSheet : unsupportedFunctionsOfSheet) {
 			System.out.println(unsupportedFunctionOfSheet);
 		}
 	}
-	
-	public Collection<String> getAllSheetFunctions(XSSFSheet sheet){
+
+	public Collection<String> getAllSheetFunctions(XSSFSheet sheet) {
 		Collection<String> functionList = new ArrayList<String>();
 		for (Row row : sheet) {
 			for (Cell cell : row) {
@@ -181,16 +182,14 @@ public class ExcelFile {
 	public void evaluateFormulaAt(int x, int y) {
 		Row row = currentSheet.getRow(x);
 		Cell cell = row.getCell(y);
-		FormulaEvaluator evaluator = wb.getCreationHelper()
-				.createFormulaEvaluator();
+		FormulaEvaluator evaluator = wb.getCreationHelper().createFormulaEvaluator();
 		if (cell.getCellType() == Cell.CELL_TYPE_FORMULA) {
 			evaluator.evaluateFormulaCell(cell);
 		}
 	}
 
 	public void evaluateAllFormulas() {
-		FormulaEvaluator evaluator = wb.getCreationHelper()
-				.createFormulaEvaluator();
+		FormulaEvaluator evaluator = wb.getCreationHelper().createFormulaEvaluator();
 		evaluator.evaluateAll();
 	}
 
@@ -221,10 +220,10 @@ public class ExcelFile {
 		ExcelFile.wb = wb;
 	}
 
-	public XSSFSheet getCurrentSheet(){
+	public XSSFSheet getCurrentSheet() {
 		return currentSheet;
 	}
-	
+
 	public void setCurrentSheet(String sheetName) {
 		currentSheet = wb.getSheet(sheetName);
 	}
